e12ce585c4d201c9a50d72f5c9fba1bb943bf983
BraverGangsta510 <91769979+BraverGangsta510@users.noreply.github.com>
2021-11-26 00:32:43 +0530

    Add files via upload

--- pagereplacementAlgo/COMPARISON/function Graph() {.js
+++ pagereplacementAlgo/COMPARISON/function Graph() {.js
@@ -7,12 +7,18 @@ function Graph() {
     var all = [];
 
     var tempforFifo = [];
+    var tempForLifo = [];
     var tempForLru = [];
+    var tempForMru = [];
     var list_FIFO = [];
+    var tempForRandom = [];
     var tempforopr = [];
 
     var fault_FIFO = 0;
     var page_fault_LRU = 0;
+    var page_fault_Lifo = 0;
+    var page_fault_MRU = 0;
+    var page_fault_Random = 0;
     var Page_fault_OPT = 0;
     /*---------------------------------------------------------------------------FIFO---------------------------------------------------------*/
     for (var L_FIFO = 0; L_FIFO < array.length; L_FIFO++) {
@@ -32,7 +38,23 @@ function Graph() {
         list_FIFO = tempforFifo;
     }
     all.push(fault_FIFO)
-    
+        /*---------------------------------------------------------------------------LIFO---------------------------------------------------------*/
+    for (var i_Lifo = 0; i_Lifo < array.length; i_Lifo++) {
+        if (tempForLifo.length == 0) {
+            tempForLifo.push(array[i_Lifo]);
+            page_fault_Lifo += 1;
+        } else if (tempForLifo.includes(array[i_Lifo])) {
+
+        } else if (tempForLifo.length < frame) {
+            tempForLifo.push(array[i_Lifo]);
+            page_fault_Lifo += 1;
+        } else if (tempForLifo.length >= frame) {
+            tempForLifo.pop();
+            tempForLifo.push(array[i_Lifo]);
+            page_fault_Lifo += 1;
+        }
+    }
+    all.push(page_fault_Lifo);
     /*---------------------------------------------------------------------------LRU---------------------------------------------------------*/
     for (var i_LRU = 0; i_LRU < array.length; i_LRU++) {
         var v = array[i_LRU];
@@ -54,8 +76,45 @@ function Graph() {
         }
     }
     all.push(page_fault_LRU);
-   
-   
+    /*---------------------------------------------------------------------------MRU---------------------------------------------------------*/
+    for (var i_MRU = 0; i_MRU < array.length; i_MRU++) {
+        if (tempForMru.length == 0) {
+            tempForMru.push(array[i_MRU]);
+            page_fault_MRU += 1;
+        } else if (tempForMru.includes(array[i_MRU])) {} else if (tempForMru.length < frame) {
+            tempForMru.push(array[i_MRU]);
+            page_fault_MRU += 1;
+        } else if (tempForMru.length >= frame) {
+            for (var index = 0; index < tempForMru.length; index++) {
+                if (tempForMru[index] === array[i_MRU - 1]) {
+                    tempForMru[index] = array[i_MRU];
+                    page_fault_MRU += 1;
+                } else if (tempForMru.length > frame) {
+                    tempForMru.pop();
+                    tempForMru.push(array[i_MRU]);
+                    page_fault_MRU += 1;
+                }
+            }
+        }
+    }
+    all.push(page_fault_MRU);
+    /*---------------------------------------------------------------------------Random---------------------------------------------------------*/
+    for (var i = 0; i < array.length; i++) {
+        if (tempForRandom.length == 0) {
+            tempForRandom.push(array[i]);
+            page_fault_Random += 1;
+        } else if (tempForRandom.includes(array[i])) {
+
+        } else if (tempForRandom.length < frame) {
+            tempForRandom.push(array[i]);
+            page_fault_Random += 1;
+        } else if (tempForRandom.length >= frame) {
+            index = Math.floor(Math.random() * parseInt(frame));
+            tempForRandom[index] = array[i];
+            page_fault_Random += 1;
+        }
+    }
+    all.push(page_fault_Random);
     /*---------------------------------------------------------------------------OPR---------------------------------------------------------*/
     for (var k = 0; k < array.length; k++) {
         if (tempforopr.length === 0) {
@@ -107,7 +166,7 @@ function Graph() {
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ["FIFO", "LRU", "OPR"],
+            labels: ["FIFO", "LIFO", "LRU", "MRU", "Random", "OPR"],
             datasets: [{
                 label: "Page Fault :-",
                 data: all,
